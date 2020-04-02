61a984e63ce909e4e55e2a52820349c8b5c11e52
emddeveloper <emd.developer@gmail.com>
2020-04-02 16:55:10 +0530

    arrow function tutorals

--- scrpts.js
+++ scrpts.js
@@ -1,1 +1,36 @@
-console.log("Hello")
+//let and const
+//arrow function
+/*
+function myage(){
+    console.log('28')
+}
+myage()
+
+var myage1 = function(){
+    return 29
+}
+console.log(myage1());
+
+let myage2 = () => {
+    return 30
+}
+
+console.log(myage2());
+
+let myage3 =()=> 31;
+console.log(myage3());
+
+let myage4 =(a,b)=> a+b;
+console.log(myage4(20,12));
+
+let myage5 = a => a;
+console.log(myage5(33));
+
+setTimeout(function(){console.log('34')},1000);
+
+setTimeout(()=>console.log('35'),1000);
+*/
+
+
+
+
